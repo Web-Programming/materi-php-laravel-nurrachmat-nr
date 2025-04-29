8e446c46bb0348b6857a9bd1ea40fc96e1e47590
Nur Rachmat <rachmat.nur91@gmail.com>
2025-04-29 15:02:52 +0700

    Route dan View

---
+++ latihan/resources/views/berita.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h4>Ini Adalah Halaman Berita</h4>
+    ID: <?php echo $id; ?><br/>
+    Title: <?php echo $title; ?><br/>
+</div>
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/profil", function(){
+    return view("profil");
+});
+
+Route::get("/berita/{id}/{title?}", function($id, $title = NULL){
+    return view("berita", ['id' => $id, 'title' => $title]);
+});
+
+Route::get("/total/{bil1}/{bil2?}/{bil3?}", 
+    function($bil1, $bil2, $bil3 = 0){
+    return view("hasil", [
+        'total' => $bil1 + $bil2 + $bil3, 
+        'bil1' => $bil1, 
+        'bil2' => $bil2, 
+        'bil3' => $bil3
+    ]);
+});
